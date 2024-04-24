1749809f6841b0905165c84da4590cd986935d3b
Huu Thoai <thoainh2003.work@gmail.com>
2024-04-25 00:14:18 +0700

    Thoại flatList

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Login from './Components/Login';
 import { AuthContext } from './Context/AuthContext';
 import Home from './Components/Home';
+import storage from '@react-native-firebase/storage';
 
 export default function App() {
     const [userData, setUserData] = React.useState(null);
@@ -20,3 +21,29 @@ const styles = StyleSheet.create({
         backgroundColor: '#F6F8FC',
     },
 });
+// import { useEffect } from 'react';
+// import database from '@react-native-firebase/database';
+
+// const FetchData = () => {
+//     useEffect(() => {
+//         // Đảm bảo thay thế 'userID' bằng key cụ thể hoặc đường dẫn đến user cụ thể mà bạn muốn đọc.
+//         database()
+//             .ref('/user/userID')
+//             .once('value')
+//             .then(snapshot => {
+//                 console.log('User data: ', snapshot.val());
+//                 // Bạn có thể xử lý dữ liệu tại đây hoặc thiết lập state để cập nhật UI.
+//             })
+//             .catch(error => {
+//                 console.error('Error fetching user data:', error);
+//             });
+//     }, []);
+
+//     return (
+//         <View>
+//             <Text>Fetching Data from Firebase Realtime Database</Text>
+//         </View>
+//     );
+// };
+
+// export default FetchData;
---
+++ Components/Loading.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <View style={styles.loadingIndicator}>
+            <ActivityIndicator
+                size={'large'}
+                color={'grey'}
+            >
+            </ActivityIndicator>
+        </View>
+
+    )
+};
+const styles = StyleSheet.create({
+    loadingIndicator: {
+        position: 'absolute',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'stransparent',
+        width: '100%',
+        height: '100%',
+
+    },
+})
